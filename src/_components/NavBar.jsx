c63575a7740a814daecf8a2c17de3d7b0fe055d2
Johan Nguyen <thaithanhace@gmail.com>
2025-07-20 17:32:31 -0500

    styled nav comp

--- src/_components/NavBar.jsx
+++ src/_components/NavBar.jsx
@@ -1,20 +1,20 @@
 export default function NavBar ({}) {
     return (
-        <div className="flex justify-between items-center px-8 py-6 bg-gray-50">
+        <div className="flex justify-between items-center px-8 py-6 bg-gray-300">
             {/* Left side - Site title */}
             <div className="text-2xl font-bold text-black">
                 Rabinovitch Research Group
             </div>
             
             {/* Right side - Navigation links */}
-            <nav className="flex space-x-8">
-                <a href="#research" className="text-lg text-black hover:text-gray-600 transition-colors">
+            <nav className="flex space-x-10 px-6">
+                <a href="#research" className="text-lg text-black font-bold hover:underline">
                     Research
                 </a>
-                <a href="#students" className="text-lg text-black hover:text-gray-600 transition-colors">
+                <a href="#students" className="text-lg text-black font-bold hover:underline">
                     Current Students
                 </a>
-                <a href="#activities" className="text-lg text-black hover:text-gray-600 transition-colors">
+                <a href="#activities" className="text-lg text-black font-bold hover:underline">
                     Activities
                 </a>
             </nav>
